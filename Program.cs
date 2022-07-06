beae32affb2c010bd812b359078004d879324149
curso.ads <curso.ads@LAB06-09>
2022-07-06 19:35:36 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade5Djalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num;
+
+            for (i = 1; i <= 100; i++)
+            {
+                num = i % 2;
+                if (num == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
